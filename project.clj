068090eeccc69c280770fb360e2938f796fe9daa
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-10-15 16:37:02 -0400

    Move cljs source to src/

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject barecljs "0.1.0"
-  :source-paths ["src-clj"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1934"
                   :exclusions [org.apache.ant/ant]]]
+  :source-paths ["src"]
+
   :plugins [[lein-cljsbuild "0.3.4"]]
   :cljsbuild {
               :builds [{:source-paths ["src-cljs"]
--- src-cljs/bare/core.cljs
+++
@@ -1,3 +0,0 @@
-(ns bare.core)
-
-(js/console.log (+ 1000 1000))
